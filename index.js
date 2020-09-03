74e46cb96ecbbb7265052ec3bb2b10fdf237edb7
PeterX <peteryxu@us.ibm.com>
2020-09-03 00:03:47 -0400

    add local test

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+
+app.use(express.static('public'))
+
+app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
