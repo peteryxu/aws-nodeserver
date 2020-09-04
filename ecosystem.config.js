c400abdced39ec8c5beeef8570743192472dc264
PeterX <peteryxu@us.ibm.com>
2020-09-03 22:08:47 -0400

    changed server DNS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-14-187-242.us-east-2.compute.amazonaws.com',
+      host: 'ec2-3-133-92-165.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/KeyPair4-AWS-NodeJS-Ubuntu.pem',
       ref: 'origin/master',
       repo: 'git@github.com:peteryxu/aws-nodeserver.git',
