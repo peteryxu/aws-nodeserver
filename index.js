f363e61edbd8b7569aee137a6d1259384627b85c
PeterX <peteryxu@us.ibm.com>
2020-09-06 10:28:34 -0400

    modified on 0906

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ var app = express();
 app.use(express.static('public'))
 
 app.get('/hello', function (req, res) {
-  res.send('Hello World! 0903');
+  res.send('hello msg from 09/06');
 });
+
+app.get('/bands', function (req, res) {
+  let json={"bands":[
+    {"name":"Ten Times Pointless", "tours":10},            
+    {"name":"Scotland Thunder Brains", "tours":9},
+    {"name":"Pointlessknot", "tours":7},
+    {"name":"Torch, Ladies and Koalas", "tours":5},
+    {"name":"My Heart, Your Brains", "tours":6},
+    {"name":"Scotland State of Mind", "tours":3},
+    {"name":"Our Pointless Torch Love", "tours":8}]}; 
+
+    res.contentType('application/json');
+    res.send(json);
+});
+
+
 
 
 app.listen(8080, '0.0.0.0', function () {
